12d0c9987421d343ed2c5485c081ce1db25f276d
havii966 <havimalviya3@gmail.com>
2025-11-13 12:37:45 +0530

    first commit havi

--- src/components/DiseaseDetails.js
+++ src/components/DiseaseDetails.js
@@ -6,20 +6,22 @@ export const DiseaseDetails = [
         title: "Anxiety",
         slug:"Anxiety",
         description: "Learn about anxiety, the symptoms, causes, how to manage the condition and where you can get help.",
-        para:"anxiety ho rahi hai ab kya karu me "
+        para:"What is anxiety?<br> Anxiety is a feeling of worry, nervousness, or fear about an event or situation. It is a normal reaction to stress. Anxiety can help you stay alert for a challenging situation at work, study harder for an exam, or remain focused on an important speech. In general, it helps you cope. When it becomes hard to control and affects your day-to-day life, it can be disabling.  "
 
     },
     {
         image: "../assets/Women-holding-2.jpg",
         title: "Bladder health",
         slug:"Bladder health",
-        description: "Learn about the bladder and pelvic floor muscles, and conditions that affect the bladder such as urinary incontinence."
+        description: "Learn about the bladder and pelvic floor muscles, and conditions that affect the bladder such as urinary incontinence.",
+        para:"Bladder health is a vital aspect of women’s overall well-being, yet it is often overlooked in daily health routines. The bladder, a muscular organ responsible for storing and releasing urine, can be affected by various factors including age, hormonal changes, childbirth, and lifestyle habits. Common bladder issues in women include urinary tract infections (UTIs), overactive bladder (OAB), urinary incontinence, and interstitial cystitis. Studies show that women are more susceptible to these problems due to their shorter urethra, which makes it easier for bacteria to enter, as well as pelvic floor weakening from pregnancy or menopause. Maintaining bladder health involves practicing good hygiene, staying well-hydrated, performing pelvic floor exercises, and avoiding bladder irritants such as caffeine and alcohol. Research also highlights the role of the urinary microbiome and how chronic conditions like diabetes or obesity can influence bladder function. Promoting awareness about bladder health can empower women to recognize early symptoms, seek timely medical care, and adopt preventive measures to improve their quality of life."
     },
      {
         image: "../assets/Woman_jogging_bone_health_3.jpg",
         title: "Bone health",
         slug:"Bone health",
-        description: "Learn how to look after your bones and reduce the risk of developing osteoporosis."
+        description: "Learn how to look after your bones and reduce the risk of developing osteoporosis.",
+        para:"Maintaining bone health requires a combination of proper nutrition, exercise, and lifestyle choices. A diet rich in calcium (found in dairy products, leafy greens, and fortified foods) and vitamin D (from sunlight, eggs, and fish) is essential for bone strength and calcium absorption. Weight-bearing exercises like walking, jogging, and resistance training help stimulate bone growth and maintain bone density. Hormonal balance also plays a vital role; therefore, managing menopause-related changes and consulting healthcare providers about supplements or therapy can help. Preventive steps taken early in life — such as healthy eating, avoiding smoking, and staying active — can significantly reduce the risk of fractures and osteoporosis later on. Promoting bone health awareness among women encourages proactive habits that support strength, mobility, and independence throughout life."
     },
      {
         image: "../assets/toilet_roll-4.jpg",
@@ -31,89 +33,105 @@ export const DiseaseDetails = [
         image: "../assets/Breast_check1-5.jpg",
         title: "Breast health",
         slug:"Breast health",
-        description: "Learn more about breasts, common breast conditions and important health checks."
+        description: "Learn more about breasts, common breast conditions and important health checks.",
+        para:"Breast health is an essential aspect of women’s overall well-being, encompassing awareness, prevention, and early detection of potential issues. The breasts undergo various changes throughout a woman’s life due to hormonal fluctuations during puberty, menstruation, pregnancy, breastfeeding, and menopause. Maintaining breast health involves understanding what is normal for one’s body, practicing self-examinations, and seeking regular medical screening"
     },
      {
         image: "../assets/endometriosis-6.jpg",
         title: "Endometriosis",
         slug:"Endometriosis",
-        description: "Learn more about endometriosis, including the symptoms, causes and treatments."
+        description: "Learn more about endometriosis, including the symptoms, causes and treatments.",
+        para:"Endometritis is an inflammation or infection of the inner lining of the uterus, known as the endometrium. It is most commonly caused by a bacterial infection that can occur after childbirth, miscarriage, abortion, or certain gynecological procedures such as a dilation and curettage (D&C) or insertion of an intrauterine device (IUD). In rare cases, it may also occur as part of a pelvic inflammatory disease (PID) when bacteria spread from the vagina or cervix into the uterus."
     },
      {
         image: "../assets/woman_happy_7.jpg",
         title: "Healthy ageing",
         slug:"Healthy ageing",
-        description: "How to look your physical and mental health as you age."
+        description: "How to look your physical and mental health as you age.",
+        para:"Healthy aging in women involves maintaining physical, mental, and emotional well-being throughout the later stages of life. As women age, they experience natural biological changes such as menopause, decreased bone density, hormonal shifts, and slower metabolism. These changes can increase the risk of chronic conditions like osteoporosis, heart disease, diabetes, and cognitive decline. However, adopting a proactive and balanced lifestyle can help women age gracefully and maintain a high quality of life."
     },
      {
         image: "../assets/health-checks-at-8.jpg",
         title: "Health checks",
         slug:"Health checks",
-        description: "Learn about key health measures and important health checks for women at different stages of your life."
+        description: "Learn about key health measures and important health checks for women at different stages of your life.",
+        para:"egular health checks are essential for women to detect potential health issues early, manage risk factors, and maintain long-term well-being. Each stage of a woman’s life — from adolescence to older adulthood — brings different health priorities, making preventive screenings and checkups a key part of healthy living."
     },
      {
         image: "../assets/eating-to-lower-blood-pressure-9.jpg",
         title: "Heart health",
         slug:"Heart health",
-        description: "Learn about cardiovascular disease, the additional risk factors for women and how you can look after your heart."
+        description: "Learn about cardiovascular disease, the additional risk factors for women and how you can look after your heart.",
+        para:"As women age, hormonal changes — particularly the decline in estrogen after menopause — can increase the risk of heart disease by raising blood pressure, cholesterol, and fat accumulation in the arteries. Other key risk factors include smoking, obesity, diabetes, high blood pressure, high cholesterol, stress, and a sedentary lifestyle. Family history and certain conditions unique to women, such as pregnancy-related hypertension or gestational diabetes, can also elevate risk later in life."
     },
      {
         image: "../assets/Menopause-web-10.jpg",
         title: "Menopause",
         slug:"Menopause",
-        description: "Learn about menopause,the symptoms, causes, management options and where you can get help."
+        description: "Learn about menopause,the symptoms, causes, management options and where you can get help.",
+        para:"Menopause occurs because the ovaries gradually produce less estrogen and progesterone, the hormones that regulate menstruation and fertility. This hormonal decline leads to the cessation of ovulation and menstruation, bringing a range of physical and emotional changes. Common symptoms include hot flashes, night sweats, irregular periods, mood swings, fatigue, vaginal dryness, and sleep disturbances. Some women may also experience weight gain, thinning hair, or decreased bone density."
     },
      {
         image: "../assets/woman-next-to-11.jpg",
         title: "Migraine",
         slug:"Migraine",
-        description: "Understand the difference between migraine and headache, key triggers and different treatments."
+        description: "Understand the difference between migraine and headache, key triggers and different treatments.",
+         para:"Migraine is a type of recurrent, severe headache often accompanied by nausea, vomiting, and sensitivity to light and sound. It affects women three times more often than men, mainly due to hormonal fluctuations involving estrogen. Migraines can last from a few hours to several days and can significantly impact daily life and productivity.n women, migraines are closely linked to the menstrual cycle. Many experience menstrual migraines, which occur just before or during their period when estrogen levels drop. Hormonal changes during pregnancy, perimenopause, or menopause, and the use of oral contraceptives, can also trigger or worsen migraine episodes. Other common triggers include stress, lack of sleep, dehydration, skipping meals, strong smells, caffeine, alcohol, and certain foods (like cheese or chocolate)."
     },{
         image: "../assets/Supplements-12.jpg",
         title: "Natural therapies",
         slug:"Natural therapies",
-        description: " What is a natural therapy complementary therapy complementary medicine and alternative therapy ?"
+        description: " What is a natural therapy complementary therapy complementary medicine and alternative therapy ?",
+         para:"Natural therapies, also known as complementary or alternative treatments, focus on supporting the body’s natural ability to heal and maintain balance. For women, these therapies can help manage hormonal changes, menstrual discomfort, stress, menopause symptoms, and overall well-being — often alongside conventional medical care."
     },
     {
         image: "../assets/Nutritionist_with_patient-13.jpg",
         title: "Nutrition and healthy living",
         slug:"Nutrition and healthy living",
-        description: "Eating and drinking for good health, including important nutrients....."
+        description: "Eating and drinking for good health, including important nutrients.....",
+         para:"Nutrition and healthy living are the foundations of overall well-being and disease prevention, especially for women whose nutritional needs change throughout life. A balanced diet provides the essential nutrients the body needs to function properly, maintain energy, support hormonal balance, and promote reproductive and bone health. Women should focus on consuming a variety of fruits, vegetables, whole grains, lean proteins, and healthy fats, while limiting processed foods, sugar, and salt."
     },{
         image: "../assets/vulva-booklet-illustration-14.jpg",
         title: "Ovaries and uterus",
         slug:"Ovaries and uterus",
-        description: "Health conditions, medical procedures and key terms that relate to the ovaries and uterus."
+        description: "Health conditions, medical procedures and key terms that relate to the ovaries and uterus.",
+         para:"The ovaries are two small, almond-shaped glands located on either side of the uterus. They have two primary functions — producing eggs (ova) and secreting female hormones, mainly estrogen and progesterone, which regulate the menstrual cycle, support pregnancy, and influence secondary sexual characteristics such as breast development. Each month, one ovary releases a mature egg in a process called ovulation, which is essential for conception"
     },{
         image: "../assets/PCOS-web-content_15.jpg",
         title: "Polycystic ovary syndrome (PCOS)",
         slug:"Polycystic ovary syndrome (PCOS)",
-        description: "PCOS affects one in 10 women and is more common in some high-risk groups ...."
+        description: "PCOS affects one in 10 women and is more common in some high-risk groups ....",
+         para:"Polycystic Ovary Syndrome (PCOS) is a common hormonal disorder that affects women of reproductive age. It occurs when the ovaries produce higher-than-normal levels of androgens (male hormones), which can interfere with ovulation and cause multiple small cysts (fluid-filled sacs) to develop on the ovaries.The exact cause of PCOS is not fully understood, but factors such as genetics, insulin resistance, inflammation, and hormonal imbalance play key roles. Women with PCOS often experience a variety of symptoms, including irregular or absent periods, excessive hair growth (hirsutism), acne, weight gain, hair thinning, and difficulty conceiving. Some may also develop darkened skin patches around the neck or underarms."
     },{
         image: "../assets/calendar-monthly-cycle_17.jpg",
         title: "Periods",
         slug:"Periods",
-        description: "Your menstrual cycle is a process that prepares your body for possible pregnancy."
+        description: "Your menstrual cycle is a process that prepares your body for possible pregnancy.",
+         para:"Menstruation, commonly known as a period, is a natural and essential part of a woman’s reproductive cycle. It involves the shedding of the uterine lining (endometrium) through the vagina when pregnancy does not occur. This process is controlled by hormones such as estrogen and progesterone and typically occurs every 21 to 35 days, lasting for about 3 to 7 days During each cycle, the body prepares for a potential pregnancy. The ovaries release an egg (ovulation), and the uterine lining thickens to support it. If the egg isn’t fertilized, hormone levels drop, causing the lining to break down and exit the body as menstrual blood. Normal menstrual flow varies among women — it can range from light to heavy and may include mild cramps, bloating, mood swings, or fatigue.."
     },{
         image: "../assets/woman-tattoos-18.jpg",
         title: "Persistent pelvic pain",
         slug:"Persistent pelvic pain",
-        description: "How it develops, where you can get help and what you can do to manage your pain."
+        description: "How it develops, where you can get help and what you can do to manage your pain.",
+         para:"Persistent pelvic pain (PPP) is defined as chronic pain in the lower abdomen or pelvis lasting for six months or longer. It can range from mild and intermittent to severe and constant, significantly affecting a woman’s quality of life, daily activities, sexual health, and emotional well-being. Because the pelvis houses multiple organs — including the uterus, ovaries, bladder, bowel, and pelvic muscles — identifying the exact cause can be complex, as several conditions may overlap."
     },{
         image: "../assets/two_feet_in_bed_painful-18.jpg",
         title: "Sex and sexual health",
         slug:"Sex and sexual health",
-        description: "Sex and sexuality play an important role in your health and wellbeing throughout your life."
+        description: "Sex and sexuality play an important role in your health and wellbeing throughout your life.",
+         para:"A woman’s sexual health is influenced by many factors, including hormonal changes, mental health, relationship satisfaction, and cultural or societal attitudes. During different life stages — puberty, reproductive years, pregnancy, and menopause — hormonal fluctuations can affect sexual desire, arousal, and comfort. For instance, menopause often brings vaginal dryness and reduced libido due to decreased estrogen, while stress, anxiety, or depression can lower sexual interest or satisfaction at any age."
     },{
         image: "../assets/Menstrual-cycle-and-sleep-Sleep-19.jpg",
         title: "Sleep",
         slug:"Sleep",
-        description: "We spend about a third of our lives sleeping, yet many of us have problems with sleep from time to time."
+        description: "We spend about a third of our lives sleeping, yet many of us have problems with sleep from time to time.",
+         para:"Throughout a woman’s life, hormones such as estrogen and progesterone influence sleep quality. During menstruation, fluctuations may cause insomnia, fatigue, or discomfort. Pregnancy can bring frequent awakenings due to hormonal shifts, physical discomfort, or the need to urinate often. In menopause, declining estrogen levels can lead to hot flashes, night sweats, and insomnia, significantly affecting rest. Stress, anxiety, depression, and caregiving responsibilities also contribute to poor sleep in many women."
     },{
         image: "../assets/women-smiling-20.jpg",
         title: "Vulva and vagina",
         slug:"Vulva and vagina",
-        description: "Are you experiencing vulval or vaginal symptoms, such as irritation, pain or a change that’s not normal for you? "
+        description: "Are you experiencing vulval or vaginal symptoms, such as irritation, pain or a change that’s not normal for you? ",
+         para:"The vulva and vagina are important parts of the female reproductive system that play key roles in protection, sexual function, and childbirth. Though the terms are often used interchangeably, they refer to different structures. The vulva is the external genital area — including the labia majora and minora (outer and inner lips), clitoris, vaginal opening, and urethral opening — while the vagina is the internal muscular canal that connects the uterus to the outside of the body."
     },
     
 ];
